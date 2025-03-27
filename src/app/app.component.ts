4dc83f2f4e5261507c1f3be5126c52444d1565c3
ViniShuet <vinishuet@gmail.com>
2025-03-26 21:50:37 -0300

    Exercicio

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { FirstComponentComponent } from './components/first-component/first-component.component';
-
+import { NewComponentComponent } from './components/new-component/new-component.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FirstComponentComponent],
+  imports: [RouterOutlet, FirstComponentComponent, NewComponentComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
